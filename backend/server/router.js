fbc0d4843b7fb14e41be46c94790ba43fbb370a0
Erjoy Constantine Robles <elrobles1@up.edu.ph>
2024-05-08 12:37:26 +0800

    feat: setup basic server

--- backend/server/router.js
+++ backend/server/router.js
@@ -2,19 +2,37 @@ import express from "express";
 
 
 
+const router = express.Router();
 
+// DEFAULT API
+
+router.get("/", (req, res) => {
+    res.send({
+        "message": "Welcome to the home page"  
+    });
+});
 
 
 // USER API
 
-// PRODUCT API
+router.get("/api/get-all-users",);
+router.post("/post/create-user",);
 
+// PRODUCT API
+router.get("/api/get-all-products",);
 
 
 // SHOPPING CART API
+router.get("/api/get-all-products-in-cart",);
+router.post("/api/confirm-order",);
 
 
 
 // TRANSACTION API
 
+router.get("/api/get-all-transactions",);
+router.post("/api/complete-transaction",);
 
+export {
+    router
+}
