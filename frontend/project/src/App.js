7e3f588bb1062fac65eb181e0ebca4f0340a7480
Jerico <jtsanchez9@up.edu.ph>
2024-05-13 23:09:08 +0800

    feat: added initial page draft (admin/salesreport)

--- frontend/project/src/App.js
+++ frontend/project/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+const menus = [
+  { name: "Product Statistics", url: "#", id: 1 },
+  { name: "Weekly", url: "#", id: 2 },
+  { name: "Monthly", url: "#", id: 3 },
+  { name: "Annualy", url: "#", id: 4 },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Sales Report</h1>
+        <Menu menus={menus} />
       </header>
+      <Footer />
     </div>
   );
 }
