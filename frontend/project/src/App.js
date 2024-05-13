9a181dbab172b99ce6c43505000716f30500a0e2
Jerico <jtsanchez9@up.edu.ph>
2024-05-13 23:47:43 +0800

    feat: added intial page draft for admin (orderfulfillment)

--- frontend/project/src/App.js
+++ frontend/project/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+const menus = [
+  { name: "Pending", url: "#", id: 1 },
+  { name: "Confirmed", url: "#", id: 2 },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Order Fulfillment</h1>
+        <Menu menus={menus} />
       </header>
+      <Footer />
     </div>
   );
 }
