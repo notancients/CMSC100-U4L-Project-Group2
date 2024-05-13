88ac769152af5a6383836edc691b9d868a9d7c0a
Jerico <jtsanchez9@up.edu.ph>
2024-05-13 23:48:29 +0800

    feat: added components for initial page draft for admin (orderfulfillment)

---
+++ frontend/project/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>The Customer is Always Right! </p>
+      <p>Email: farm2table.support@gmail.com</p>
+      <p>Facebook: Farm 2 Table</p>
+      <p>X: @Farm2Table</p>
+      <p>Instagram: Farm2Table</p>
+    </footer>
+  );
+}
+
+export default Footer;
