7c723ac683d93c286e708ddeb715475b80949ddc
KLeoncio <kbleoncio@up.edu.ph>
2024-05-14 23:05:33 +0800

    feat: added admin landing screen

--- frontend/project/src/components/AdminLandingPage.js
+++ frontend/project/src/components/AdminLandingPage.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/adminlanding.css';
+import logo from '../images/Logo.png';
+import userIcon from '../images/user_icon.png';
+import listingMenu from '../images/listing.png';
+import accountsMenu from '../images/accounts.png';
+import pendingMenu from '../images/pending.png';
+import salesMenu from '../images/sales.png';
 
 function AdminLandingPage() {
   return (
-    <div>
-      <h1>Welcome to the Admin Landing Page!</h1>
-      {/* Add content specific to admin landing page */}
-    </div>
+    <div className="admin-page">
+      <div className="logo">
+      <Link to="/"> 
+          <img src={logo} alt="Logo Here" className="logo-img" />
+        </Link>
+        </div> 
+      <div className="nav-bar">
+        <Link to="/devs" className="nav-link">THE DEVELOPERS</Link>
+        <Link to="/whatwedo" className="nav-link">WHAT WE DO</Link>
+        <Link to="/about" className="nav-link">ABOUT</Link>
+        <Link to="/user-profile" className="user-profile">
+          <img src={userIcon} alt="User icon" className="user-icon" />
+        </Link>
+      </div>
+      <div className="menu1">
+        <Link to="/productlisting">
+        <img src={listingMenu} alt="Listing" className="listing" />
+        </Link>
+        <Link to="/accounts">
+        <img src={accountsMenu} alt="Accounts" className="accounts" />
+        </Link>
+      </div>
+      <div className="menu2">
+        <Link to="/pending">
+        <img src={pendingMenu} alt="Pending" className="pending" />
+        </Link>
+        <Link to="/sales">
+        <img src={salesMenu} alt="Sales" className = "sales" />
+        </Link>
+      </div>
+  </div>
   );
 }
 
--- frontend/project/src/components/landingPage.js
+++ frontend/project/src/components/landingPage.js
@@ -9,31 +9,33 @@ import userIcon from '../images/user_icon.png';
 
 function LandingPage() {
   return (
-    <div className="lp-container">
-      <div className="bokeh-overlay-lp">
-        <div className="bokeh-lp"></div>
-        <div className="bokeh-lp"></div>
-        <div className="bokeh-lp"></div>
-      </div>
-      <div className="nav-bar">
-        <Link to="/devs" className="nav-link">THE DEVELOPERS</Link>
-        <Link to="/whatwedo" className="nav-link">WHAT WE DO</Link>
-        <Link to="/about" className="nav-link">ABOUT</Link>
-        <Link to="/user-profile" className="user-profile">
-          <img src={userIcon} alt="User icon" className="user-icon" />
-        </Link>
-      </div>
-      <img src={lpLogo} alt="lp_logo" className="lp-logo" />
-      <div className="lp-title">
-        <img src={landingPageTitle} alt="Landing Page Background" />
-      </div>
-      <div className='button-container'>
-        <Link to="/login/user">
-          <img src={userLoginImage} alt="User Login" className="user-button" />
-        </Link>
-        <Link to="/admin">
-          <img src={adminLoginImage} alt="Admin Login" className="admin-button" />
-        </Link>
+    <div className="gradient-background">
+      <div className="lp-container">
+        <div className="bokeh-overlay-lp">
+          <div className="bokeh-lp"></div>
+          <div className="bokeh-lp"></div>
+          <div className="bokeh-lp"></div>
+        </div>
+        <div className="nav-bar">
+          <Link to="/devs" className="nav-link">THE DEVELOPERS</Link>
+          <Link to="/whatwedo" className="nav-link">WHAT WE DO</Link>
+          <Link to="/about" className="nav-link">ABOUT</Link>
+          <Link to="/user-profile" className="user-profile">
+            <img src={userIcon} alt="User icon" className="user-icon" />
+          </Link>
+        </div>
+        <img src={lpLogo} alt="lp_logo" className="lp-logo" />
+        <div className="lp-title">
+          <img src={landingPageTitle} alt="Landing Page Background" />
+        </div>
+        <div className='button-container'>
+          <Link to="/login/user">
+            <img src={userLoginImage} alt="User Login" className="user-button" />
+          </Link>
+          <Link to="/admin">
+            <img src={adminLoginImage} alt="Admin Login" className="admin-button" />
+          </Link>
+        </div>
       </div>
     </div>
   );
