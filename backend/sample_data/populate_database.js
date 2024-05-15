a6f54d9f3c001da92d6ee071d42b98344a1f9b52
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-15 18:05:00 +0800

    test: added product data population

--- backend/sample_data/populate_database.js
+++ backend/sample_data/populate_database.js
@@ -1,5 +1,7 @@
 import { registerNewUser } from "../user/user_controller.js";
+import { addProduct } from "../product/product_controller.js";
 import { USER_SAMPLE_DATA } from "./user_sample_data.js";
+import { PRODUCT_SAMPLE_DATA } from "./product_sample_data.js";
 
 async function populateUserData(userArray) {
     userArray.forEach(element => {
@@ -17,6 +19,23 @@ async function populateUserDataAPI(req, res) {
     })
 }
 
+async function populateProductData(productArray) {
+    productArray.forEach(element => {
+        addProduct(element);
+    });
+}
+
+async function populateProductAPI(req, res) {
+    console.log("Populating the product database.");
+    await populateProductData(PRODUCT_SAMPLE_DATA);
+    res.send({
+        "success":true,
+        "data": null,
+        "message": "The product database has been populated."
+    });
+}
+
 export {
-    populateUserDataAPI
+    populateUserDataAPI,
+    populateProductAPI
 }
