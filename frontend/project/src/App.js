9d7506c79a9217b2892bde51d204ed12e92e4ec2
Erjoy Constantine Robles <elrobles1@up.edu.ph>
2024-05-15 20:45:37 +0800

    feat: initialized frontend production

--- frontend/project/src/App.js
+++ frontend/project/src/App.js
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
-
-const menus = [
-  { name: "Product Statistics", url: "#", id: 1 },
-  { name: "Weekly", url: "#", id: 2 },
-  { name: "Monthly", url: "#", id: 3 },
-  { name: "Annualy", url: "#", id: 4 },
-];
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>Sales Report</h1>
-        <Menu menus={menus} />
+        <h1>Home</h1>
       </header>
-      <Footer />
     </div>
   );
 }
