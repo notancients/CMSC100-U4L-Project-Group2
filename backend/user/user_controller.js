37b25ce7bc6b2a7c2922cffe0d6318f531c4f745
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-15 11:06:13 +0800

    feat: added create user to controller

--- backend/user/user_controller.js
+++ backend/user/user_controller.js
@@ -1,5 +1,73 @@
 import { Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import UserModel from './user_model';
 
-async function registerNewUser({fname, mname, lname, email, password}) {
+const SALT = 10;
+
+async function registerNewUser({first_name, middle_name, last_name, email, password}) {
+    console.log("Registering a new user.");
+    
+    const existing_user_response = {
+        "success": false,
+        "data": null,
+        "message": "A user already exists for that email."
+    };
+
+
+    try{
+        const existing_user = await User.findOne({ email: user.email });
+
+        if (existing_user) {
+            return existing_user_response;
+        }
+
+        const encryptedPassword = bcrypt.hash(password, SALT);
+
+        const newUser = await UserModel(
+            {
+                "first_name": first_name,
+                "middle_name": middle_name,
+                "last_name": last_name,
+                "email": email,
+                "password": encryptedPassword,
+                "user_type": "User",
+            }
+        )
+
+        const created_account = await UserModel.save(newUser);
+
+        return {
+            success: true, 
+            data: created_account, 
+            message:[]
+        }
+
+    } catch (err) {
+        console.log('error: ', err);
+        return {
+            success: false, 
+            data: null, 
+            message:['An error occured while creating a cause',err]
+        };
+    }
+}
+
+async function createAdmin({fname, mname, lname, email, password}) {
+    
+}
+
+async function getAllUsers() {
+
+}
+
+async function logIn({email, password}) {
+    console.log("User is logging in.")
+}
+
+async function deleteUser() {
+
+}
+
+async function getUser() {
 
 }
--- backend/user/user_model.js
+++ backend/user/user_model.js
@@ -1,30 +1,16 @@
 import mongoose from "mongoose";
 
-const User = mongoose.model('User',
-    {
-      "fname":String,
-      "mname":String,
-      "lname":String,
-      "userType":String,
-      "email":String,
-      "password":String
-    }, 'userData'
-)
-
-export {User};
-
 const userSchema = new mongoose.Schema({
   // Schema of the users model
   first_name: {type: String, require: true},
   middle_name: {type: String, require: true},
   last_name: {type: String, require: true},
-  user_type:String,
   email: {type: String, required: true},
   password: {type: String, minlength: 8, required: true},
   user_type: {type: String, required: true, enum: ["User", "Admin"]},
 });
 
-const userModel = new mongoose.Model('Users', userSchema);
+const UserModel = new mongoose.Model('Users', userSchema);
 
 
-export default userModel;
+export default UserModel;
