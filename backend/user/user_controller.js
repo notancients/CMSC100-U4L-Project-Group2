1040fb5446bb0e64d1ff025618fbaea0b78d1cef
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-15 10:19:59 +0800

    refactor: changed user model to use schema

---
+++ backend/user/user_controller.js
@@ -0,0 +1,5 @@
+import { Model } from 'mongoose';
+
+async function registerNewUser({fname, mname, lname, email, password}) {
+
+}
--- backend/user/user_model.js
+++ backend/user/user_model.js
@@ -12,3 +12,19 @@ const User = mongoose.model('User',
 )
 
 export {User};
+
+const userSchema = new mongoose.Schema({
+  // Schema of the users model
+  first_name: {type: String, require: true},
+  middle_name: {type: String, require: true},
+  last_name: {type: String, require: true},
+  user_type:String,
+  email: {type: String, required: true},
+  password: {type: String, minlength: 8, required: true},
+  user_type: {type: String, required: true, enum: ["User", "Admin"]},
+});
+
+const userModel = new mongoose.Model('Users', userSchema);
+
+
+export default userModel;
