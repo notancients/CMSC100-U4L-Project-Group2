f39461f985c40cbe4c35dbd3a33599f8759f87a8
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-15 17:07:22 +0800

    refactor: changed product model from basic model to schema

--- backend/products/products_model.js
+++ backend/products/products_model.js
@@ -1,17 +1,13 @@
 import mongoose from 'mongoose';
 
 
-const Product = mongoose.model(
-    'Product',
-    {
-        "productId": String,
-        "productName": String,
-        "productDescription": String,
-        "productType": Number,      // (eg: 1 Crop / 2 Poultry)
-        "productQuantity": Number
+const productSchema = new mongoose.Schema({
+    // Schema of the users model
+    product_name: {type: String, required: true},
+    product_description: {type: String, required: true},
+    product_type: {type: String, required: true},
+    product_quantity: {type: Integer, required: true},
+    price: {type: Integer, required: true}
+});
 
-    },
-    'ProductData'
-);
-
-export {Product};
+export default mongoose.model('products', productSchema);
