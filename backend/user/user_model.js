5c0dd77dc7668df000e49526fdc6065f9862c782
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-15 17:07:01 +0800

    fix: changed require -> required

--- backend/user/user_model.js
+++ backend/user/user_model.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   // Schema of the users model
-  first_name: {type: String, require: true},
-  middle_name: {type: String, require: true},
-  last_name: {type: String, require: true},
+  first_name: {type: String, required: true},
+  middle_name: {type: String, required: true},
+  last_name: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, minlength: 8, required: true},
   user_type: {type: String, required: true, enum: ["User", "Admin"]},
