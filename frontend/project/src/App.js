c96c3cb26b2e7123ad5a63304f4ddc39b41f07aa
Jerico <jtsanchez9@up.edu.ph>
2024-05-18 16:26:48 +0800

    feat: added routes to Sales page

--- frontend/project/src/App.js
+++ frontend/project/src/App.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
-
-const menus = [
-  { name: "Product Statistics", url: "#", id: 1 },
-  { name: "Weekly", url: "#", id: 2 },
-  { name: "Monthly", url: "#", id: 3 },
-  { name: "Annualy", url: "#", id: 4 },
-];
+import SalesPage from './components/SalesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Sales Report</h1>
-        <Menu menus={menus} />
-      </header>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<SalesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
