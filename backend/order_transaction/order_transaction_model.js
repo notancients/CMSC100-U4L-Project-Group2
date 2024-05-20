4cb222bcb029be6e5426ef87a4f5e52596b9e124
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-20 11:40:03 +0800

    refactor: changed order transaction from model to schema

--- backend/order_transaction/order_transaction_model.js
+++ backend/order_transaction/order_transaction_model.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose'
 
-const orderTransaction = mongoose.model(
-    'ordertransaction',
-    {
-        "transactionid": String,
-        "productid": String,
-        "orderquantity": Number,
-        "orderstatus": Number,
-        "email": String,
-        "dateordered": Date,
-        //"time":TimeRanges,
-        timestamps: true,
-    },
-    "ordertransactiondatas"
-)
+const ORDER_STATUS_ENUM = ["Completed", "Cancelled", "Pending"];
 
-export {orderTransaction}
+const submittedOrderSchema = new mongoose.Schema({
+    "product_id": {type: String, required:true},
+    "product_quantity": {type: Number, required: true}
+}) 
+
+const orderTransactionSchema = new mongoose.Schema({
+    // Schema of the order transaction model
+    "user_id": {type: mongoose.Schema.ObjectId, required: true},
+    "order_status" : {type: String, required: true, enum: ORDER_STATUS_ENUM},
+    "order" : [submittedOrderSchema],
+    timestamps: true
+});
+
+export default mongoose.model('ordertransaction', orderTransactionSchema);
