c22ebbcf38a48c294fe790ac431ec1be5f33d8e3
Erjoy Robles <elrobles1@up.edu.ph>
2024-05-29 17:07:25 +0800

    fix: fixed user product add to cart after folder structure change

--- frontend/project/src/user/Products/user_products.js
+++ frontend/project/src/user/Products/user_products.js
@@ -1,27 +1,43 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/user_products.css';
-import { PRODUCT_SAMPLE_DATA } from './ProductSample';
 import logo from '../../images/Logo.png';
 import userIcon from '../../images/user_icon.png';
 import CustomCursor from '../../components/customCursor';
+import axios from "axios";
 
 
-function UserProductsPage({ cart, setCart, cartQuantity }) {
-    const [products, setProducts] = useState(PRODUCT_SAMPLE_DATA);
+function UserProductsPage({ cart, setCart }) {
+    const [products, setProducts] = useState([]);
     const [sortCriteria, setSortCriteria] = useState('');
     const [sortDirection, setSortDirection] = useState('asc');
     const [selectedType, setSelectedType] = useState('All'); 
     const [notification, setNotification] = useState({ message: '', visible: false });
 
-
-    const handleAddToCart = (productToAdd) => {
-        const existingItem = cart.find(item => item.productId === productToAdd.productId);
+    useEffect( () => {
+        console.log("Fetching products data.");
+        const fetch_data = async () => {
+            try {
+                const response = await axios.get("http://localhost:3001/api/get-all-products");
+                console.log(response.data.data);
+                setProducts(response.data.data);
+            } catch (err) {
+                console.log(err);
+                setProducts([]);
+            }
+        }
     
+        fetch_data();
+    }, []);
+
+
+    const handleAddToCart = async (productToAdd) => {
+        console.log("Handling add to cart.");
+        const existingItem = cart.find(item => item._id === productToAdd._id);
+
         if (existingItem) {
-            const updatedCart = cart.map(item => {
-                if (item.productId === productToAdd.productId) {
+            const updatedCart = cart.map( async (item) => {
+                if (item._id === productToAdd._id) {
                     return { ...item, quantity: item.quantity + 1 };
                 } else {
                     return item;
@@ -31,8 +47,21 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
         } else {
             setCart([...cart, { ...productToAdd, quantity: 1 }]);
         }
+
+        try {
+            console.log("Adding product to cart database.");
+            const response = await axios.post('http://localhost:3001/api/add-to-cart', {
+                "user_id" : "66443306653ccde666260bfb",
+                "product_id" : productToAdd._id,
+                "quantity" : 1
+            });
+            console.log(response);
+            
+        } catch (err) {
+            console.log(err);
+        }
     
-        setNotification({ message: `${productToAdd.productName} added to cart!`, visible: true });
+        setNotification({ message: `${productToAdd.product_name} added to cart!`, visible: true });
     
         setTimeout(() => {
             setNotification({ message: '', visible: false });
@@ -47,7 +76,7 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
         setSortDirection(newDirection);
 
         const sortedProducts = [...products].sort((a, b) => {
-            if (criteria === 'productName' || criteria === 'productType') {
+            if (criteria === 'product_name' || criteria === 'product_type') {
                 return newDirection === 'asc' ? a[criteria].localeCompare(b[criteria]) : b[criteria].localeCompare(a[criteria]);
             } else {
                 return newDirection === 'asc' ? a[criteria] - b[criteria] : b[criteria] - a[criteria];
@@ -66,7 +95,7 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
     };
     
 
-    const filteredProducts = selectedType === 'All' ? products : products.filter(product => product.productType === selectedType);
+    const filteredProducts = selectedType === 'All' ? products : products.filter(product => product.product_type === selectedType);
 
     const renderSortIndicator = (criteria) => {
         if (sortCriteria === criteria) {
@@ -76,7 +105,7 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
     };
 
     const renderProductTypesDropdown = () => {
-        const uniqueTypes = ['All', ...new Set(products.map(product => product.productType))];
+        const uniqueTypes = ['All', ...new Set(products.map(product => product.product_type))];
         return (
             <select className="custom-dropdown" value={selectedType} onChange={(e) => handleFilterByType(e.target.value)}>
                 {uniqueTypes.map((type, index) => (
@@ -90,11 +119,11 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
         <div className="user-products-page">
             <CustomCursor />
             {notification.visible && (
-           <div className="notification">
-           <i className="fas fa-check notification-icon"></i>
-           <span className="notification-message">{notification.message}</span>
-       </div>
-       
+            <div className="notification">
+            <i className="fas fa-check notification-icon"></i>
+            <span className="notification-message">{notification.message}</span>
+        </div>
+        
         
         )}
             <div className="logo">
@@ -112,15 +141,15 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
                 </Link>
             </div>
             <div className="title-container">
-              <h1>Our Products</h1>
+                <h1>Our Products</h1>
             </div>
             <div className="sort-and-filter-container">
                 <div className="sort-by-group">
                     <p className="sort">SORT:</p>
-                    <button className="sort" onClick={() => handleSort('productName')}>NAME {renderSortIndicator('productName')}</button>
-                    <button className="sort" onClick={() => handleSort('productType')}>TYPE {renderSortIndicator('productType')}</button>
+                    <button className="sort" onClick={() => handleSort('product_name')}>NAME {renderSortIndicator('product_name')}</button>
+                    <button className="sort" onClick={() => handleSort('product_type')}>TYPE {renderSortIndicator('product_type')}</button>
                     <button className="sort" onClick={() => handleSort('price')}>PRICE {renderSortIndicator('price')}</button>
-                    <button className="sort" onClick={() => handleSort('productQuantity')}>QUANTITY {renderSortIndicator('productQuantity')}</button>
+                    <button className="sort" onClick={() => handleSort('quantity')}>QUANTITY {renderSortIndicator('quantity')}</button>
                 </div>
                 <div className="type-filter">
                     <p className="filter">FILTER:  {renderProductTypesDropdown()}</p>
@@ -129,16 +158,16 @@ function UserProductsPage({ cart, setCart, cartQuantity }) {
 
             <div className="user-product-list">
             {filteredProducts.map((item) => (
-                    <div key={item.productId} className="item">
+                    <div key={item._id} className="item">
                         <div className="content">
-                            <img src={item.img} alt={item.productName} className='product-img'/>
-                            <p className="name">{item.productName}</p>
-                            <p className="desc">{item.productDescription}</p>
+                            <img src={item.img} alt={item.product_name} className='product-img'/>
+                            <p className="name">{item.product_name}</p>
+                            <p className="desc">{item.product_description}</p>
                         </div>
                         <div className="details">
                             <div className='price-and-stock'>
                             <p className="user-price">₱ {item.price}</p>
-                            <p className="cur_stock">Stock: {item.productQuantity}</p>
+                            <p className="cur_stock">Stock: {item.quantity}</p>
                             </div>
                             <button className='addtocart' onClick={() => handleAddToCart(item)}>Add to cart</button>
                         </div>
