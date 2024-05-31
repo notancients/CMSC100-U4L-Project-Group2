07a82d4c238e31f491642b4c36ba07a09f9c5fe0
Jerico <jtsanchez9@up.edu.ph>
2024-05-31 16:56:54 +0800

    (not tested) added axios calls for getting user transaction and cancel order

--- frontend/project/src/user/Orders/UserOrdersList.js
+++ frontend/project/src/user/Orders/UserOrdersList.js
@@ -4,15 +4,15 @@ const UserOrdersList = ({ orders, handleCancel, currentTab }) => {
   return (
     <div className="user-order-list">
       {orders.map(order => (
-        <div className="user-order-card" key={order.transactionId}>
+        <div className="user-order-card" key={order._id}>
           <div className="order-info">
-            <p>{order.productName}</p>
-          <p>Product ID: {order.productId}</p>
-          <p>Order Quantity: {order.orderQuantity}</p>
-          <p>Date Ordered: {order.dateOrdered}</p>
+            <p>{order.product_name}</p>
+            <p>Product ID: {order.product_id}</p>
+            <p>Order Quantity: {order.order_quantity}</p>
+            <p>Date Ordered: {order.date_ordered}</p>
           </div>
           {currentTab !== 3 && handleCancel && ( // Check if not on Cancelled tab
-            <button className="user-action-button" onClick={() => handleCancel(order.transactionId)}>Cancel</button>
+            <button className="user-action-button" onClick={() => handleCancel(order._id)}>Cancel</button>
           )}
         </div>
       ))}
